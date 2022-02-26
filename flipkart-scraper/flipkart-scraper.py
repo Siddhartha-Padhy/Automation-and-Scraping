2030566b4245dd8bbe73358bda857a57e230b6cb
Siddhartha Shankar Padhy <83332618+Siddhartha-Padhy@users.noreply.github.com>
2022-02-26 23:50:58 +0530

    Update flipkart-scraper.py
    Stable version but takes a lot of time.

--- flipkart-scraper/flipkart-scraper.py
+++ flipkart-scraper/flipkart-scraper.py
@@ -1,58 +1,97 @@
-from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+import csv
 
 URL = "https://www.flipkart.com/"
 PATH = "C:/SeleniumDrivers/chromedriver.exe"
-ITEM = 'atomic habits'
 
 opts = Options()
 opts.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36")
 
-driver = webdriver.Chrome(executable_path=PATH,chrome_options=opts)
-driver.get(URL)
-driver.implicitly_wait(30)
+heads = ['Title','Desc','Rating','Price']
+data = []
 
-#cancel the login dialog box
-cancel_btn = driver.find_element(By.CSS_SELECTOR,'button[class="_2KpZ6l _2doB4z"]')
-cancel_btn.click()
-print("\nCancel Button Clicked\n")
+def create_file(data, item):
+    file = item.lower().replace(' ','_')
+    file = f'Auto-Scraping/flipkart-scraper/{file}.csv'
+    with open(file,'w', newline='') as f:
+        w=csv.DictWriter(f,heads)
+        w.writeheader()
+        for value in data:
+            w.writerow(value)
+    print("Task Complete!")
 
-#enter query in search box
-search_bar = driver.find_element(By.CLASS_NAME,'_3704LK')
-search_bar.click()
-search_bar.send_keys(ITEM + Keys.ENTER)
-print("\nSearched\n")
+def driver_func(driver,item, min_lim, max_lim):
+    #cancel the login dialog box
+    cancel_btn = driver.find_element(By.CSS_SELECTOR,'button[class="_2KpZ6l _2doB4z"]')
+    cancel_btn.click()
 
-sleep(10)
+    #enter query in search box
+    search_bar = driver.find_element(By.CLASS_NAME,'_3704LK')
+    search_bar.click()
+    search_bar.send_keys(item + Keys.ENTER)
 
-results_list = driver.find_elements(By.CSS_SELECTOR,'div[class="_13oc-S"]')
-print("Result_list length = ",len(results_list))
+    # filter price
+    filter_box = driver.find_elements(By.CLASS_NAME,'_2YxCDZ')
+    val1 = Select(filter_box[0])
+    val1.select_by_value(min_lim)
+    
+    val2 = Select(filter_box[1])
+    val2.select_by_value(max_lim)
 
-num = 1
-for quad_set in results_list:
-    product_list = quad_set.find_elements(By.CLASS_NAME,'_4ddWXP')
-    print("Product list length=",len(product_list))
+    WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.CSS_SELECTOR,'div[class="_13oc-S"]')))
 
-    for product in product_list:
-        print(num)
-        num +=1
+    results_list = driver.find_elements(By.CSS_SELECTOR,'div[class="_13oc-S"]')
 
-        title = str(product.find_element(By.CSS_SELECTOR,'a[class="s1Q9rs"]').get_attribute('innerHTML')).strip()
-        desc = str(product.find_element(By.CLASS_NAME,'_3Djpdu').get_attribute('innerHTML')).strip()
+    num = 1
+    try:
+        for quad_set in results_list:
+            product_list = quad_set.find_elements(By.CLASS_NAME,'_4ddWXP')
 
-        try:
-            rating = str(product.find_element(By.CLASS_NAME,'_3LWZlK').get_attribute('innerHTML')).strip()
-            rating = rating.split('<')[0]
-        except:
-            rating = 0
-        price = str(product.find_element(By.CLASS_NAME,'_30jeq3').get_attribute('innerHTML')).strip()
+            for product in product_list:
+                print(num)
+                num +=1
+                info = {}
 
-        print("Name: " + title)
-        print("Desc: " + desc)
-        print("Rating: " + rating)
-        print("Price: " + price)
+                info['Title'] = str(product.find_element(By.CSS_SELECTOR,'a[class="s1Q9rs"]').get_attribute('innerHTML')).strip()
+
+                try:
+                    info['Desc'] = str(product.find_element(By.CLASS_NAME,'_3Djpdu').get_attribute('innerHTML')).strip()
+                except:
+                    info['Desc'] = 'Unavailable'
+
+                try:
+                    info['Rating'] = str(product.find_element(By.CLASS_NAME,'_3LWZlK').get_attribute('innerHTML')).strip()
+                    info['Rating'] = info['Rating'].split('<')[0]
+                except:
+                    info['Rating'] = 'Unavailable'
+
+                try:
+                    info['Price'] = str(product.find_element(By.CLASS_NAME,'_30jeq3').get_attribute('innerHTML')).strip()
+                    info['Price'] = info['Price'][1:len(info['Price'])]
+                except:
+                    info['Price'] = 'Unavailable'
+
+            print(info)
+            data.append(info)
+    except:
+        pass
+
+    create_file(data, item)
+
+def main():
+    item = str(input("Enter item: "))
+    max_lim = str(input('Enter upper limit: '))
+    min_lim = str(input('Enter lower limit: '))
+    driver = webdriver.Chrome(executable_path=PATH,chrome_options=opts)
+    driver.implicitly_wait(30)
+    driver.get(URL)
+    driver_func(driver,item, min_lim=min_lim, max_lim=max_lim)
+
+if __name__=="__main__":
+    main()
